f4b5ad307e40593b9810b8acc621f76e8dc52860
Rafeef87 <rafeefkhalifah1987@gmail.com>
2025-01-24 15:46:06 +0100

    Adding Business project and Dots.

---
+++ Business/Dtos/ProductRegistrationForm.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Business.Dtos;
+
+public class ProductRegistrationForm
+{
+    [Required]
+    public string ProductName { get; set; } = null!;
+    public decimal Price { get; set; }
+}
---
+++ Business/Dtos/ProjectRegistrationForm.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Business.Dtos;
+
+public class ProjectRegistrationForm
+{
+    [Required]
+    public string ProjectName { get; set; } = null!;
+    [Required]
+
+    [Column(TypeName = "date")]
+    public DateTime StartDate { get; set; }
+
+    [Column(TypeName = "date")]
+    public DateTime EndDate { get; set; }
+}
