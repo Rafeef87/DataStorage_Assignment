95e04db134108b00cda33d6de50941e7edd0a7a4
Rafeef87 <rafeefkhalifah1987@gmail.com>
2025-01-24 15:54:32 +0100

    Create Models del.

--- Business/Dtos/ProjectRegistrationForm.cs
+++ Business/Dtos/ProjectRegistrationForm.cs
@@ -14,4 +14,16 @@ public class ProjectRegistrationForm
 
     [Column(TypeName = "date")]
     public DateTime EndDate { get; set; }
+
+    public int CustomerId { get; set; }
+    public CustomerRegistrationForm Customer { get; set; } = null!;
+
+    public int StatusId { get; set; }
+    public StatusTypeRegistrationForm Status { get; set; } = null!;
+
+    public int UserId { get; set; }
+    public UserRegistrationForm User { get; set; } = null!;
+
+    public int ProductId { get; set; }
+    public ProductRegistrationForm Product { get; set; } = null!;
 }
--- Data/Entities/ProductEntity.cs
+++ Data/Entities/ProductEntity.cs
@@ -7,5 +7,5 @@ public class ProductEntity
     [Key]
     public int Id { get; set; }
     public string ProductName { get; set; } = null!;
-    public decimal? Price { get; set; }
+    public decimal Price { get; set; }
 }
