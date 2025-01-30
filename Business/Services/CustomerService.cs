04c0012e1fbb912bcba6f55268fcc2c2394fbd20
Rafeef87 <rafeefkhalifah1987@gmail.com>
2025-01-30 17:11:05 +0100

    Adding CustomerService with CRUD.

---
+++ Business/Services/CustomerService.cs
@@ -0,0 +1,50 @@
+﻿using System.Linq.Expressions;
+using Business.Dtos;
+using Business.Factories;
+using Business.Interfaces;
+using Business.Models;
+using Data.Entities;
+using Data.Interfaces;
+
+namespace Business.Services;
+
+public class CustomerService(ICustomerRepository customerRepository) : ICustomerService
+{
+    private readonly ICustomerRepository _customerRepository = customerRepository;
+    //CREATE
+    public async Task<Customer> CreateCustomerAsync(CustomerRegistrationForm form)
+    {
+        var enttiy = await _customerRepository.GetAsync(x => x.CustomerName == form.CustomerName);
+        enttiy ??= await _customerRepository.CreateAsync(CustomerFactory.Create(form));
+
+        return CustomerFactory.Create(enttiy);
+    }
+    //READ
+    public async Task<IEnumerable<Customer>> GetAllCustomersAsync()
+    {
+        var entties = await _customerRepository.GetAllAsync();
+        var customers = entties.Select(CustomerFactory.Create);
+        return customers ?? [];
+    }
+    public async Task<Customer> GetCustomerAsync(Expression<Func<CustomerEntity, bool>> expression)
+    {
+
+        var enttiy = await _customerRepository.GetAsync(expression);
+        var customer = CustomerFactory.Create(enttiy);
+        return customer ?? null!;
+    }
+    //UPDATE
+    public async Task<Customer> UpdateCustomerAsync(CustomerUpdateForm form)
+    {
+        var updateEntity = CustomerFactory.Create(form);
+        var entity = await _customerRepository.UpdateAsync(p => p.Id == form.Id, updateEntity);
+        var customer = CustomerFactory.Create(entity);
+        return customer ?? null!;
+    }
+    //DELETE
+    public async Task<bool> DeleteCustomerAsync(int id)
+    {
+        var result = await _customerRepository.DeleteAsync(x => x.Id == id);
+        return result;
+    }
+}
--- Business/Services/ProductService.cs
+++ Business/Services/ProductService.cs
@@ -5,31 +5,32 @@ using Business.Interfaces;
 using Business.Models;
 using Data.Entities;
 using Data.Interfaces;
+using Data.Repositories;
 
 namespace Business.Services;
 
 public class ProductService(IProductRepository productRepository) : IProductService
 {
-    private readonly IProductRepository _ProductRepository = productRepository;
+    private readonly IProductRepository _productRepository = productRepository;
     //CREATE
     public async Task<Product> CreateProductAsync(ProductRegistrationForm form)
     {
-        var enttiy = await _ProductRepository.GetAsync(x => x.ProductName ==  form.ProductName);
-        enttiy ??= await _ProductRepository.CreateAsync(ProductFactory.Create(form));
+        var enttiy = await _productRepository.GetAsync(x => x.ProductName ==  form.ProductName);
+        enttiy ??= await _productRepository.CreateAsync(ProductFactory.Create(form));
 
         return ProductFactory.Create(enttiy);
     }
     //READ
     public async Task<IEnumerable<Product>> GetAllProductsAsync()
     {
-        var entties = await _ProductRepository.GetAllAsync();
+        var entties = await _productRepository.GetAllAsync();
         var products = entties.Select(ProductFactory.Create);
         return products ?? [];
     }
     public async Task<Product> GetProductAsync(Expression<Func<ProductEntity, bool>> expression)
     {
 
-        var enttiy = await _ProductRepository.GetAsync(expression);
+        var enttiy = await _productRepository.GetAsync(expression);
         var product= ProductFactory.Create(enttiy);
         return product ?? null!;
     }
@@ -37,14 +38,14 @@ public class ProductService(IProductRepository productRepository) : IProductServ
     public async Task<Product> UpdateProductAsync(ProductUpdateForm form)
     {
         var updateEntity = ProductFactory.Create(form);
-        var entity = await _ProductRepository.UpdateAsync(p => p.Id == form.Id, updateEntity);
+        var entity = await _productRepository.UpdateAsync(p => p.Id == form.Id, updateEntity);
         var product = ProductFactory.Create(entity);
         return product ?? null!;
     }
     //DELETE
     public async Task<bool> DeleteProductAsync(int id)
     {
-       var result = await _ProductRepository.DeleteAsync(x => x.Id == id);
+       var result = await _productRepository.DeleteAsync(x => x.Id == id);
         return result;
     }
 }
