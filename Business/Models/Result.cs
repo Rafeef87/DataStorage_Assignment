5ca23049e74c4792e829f69862f8cecbcc2d60ee
Rafeef87 <rafeefkhalifah1987@gmail.com>
2025-01-30 09:26:25 +0100

    Result class with Interface

---
+++ Business/Models/Result.cs
@@ -0,0 +1,12 @@
+﻿using Business.Interfaces;
+
+namespace Business.Models;
+
+public abstract class Result : IResult
+{
+    public bool Success { get; protected set; }
+
+    public int StatusCode { get; protected set; }
+
+    public string? ErrorMassage { get; protected set; }
+}
