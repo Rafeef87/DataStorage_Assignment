04d3484103794d02b633ce8881ede27e9a91cffa
Rafeef87 <rafeefkhalifah1987@gmail.com>
2025-01-30 09:53:48 +0100

    Success and Error Result

--- Business/Models/Result.cs
+++ Business/Models/Result.cs
@@ -9,4 +9,33 @@ public abstract class Result : IResult
     public int StatusCode { get; protected set; }
 
     public string? ErrorMassage { get; protected set; }
+    public static Result ok()
+    {
+        return new SuccessResult(200);
+    }
+    public static Result BadRequest(string message)
+    {
+        return new ErrorResult(400, message);
+    }
+    public static Result NotFound(string message)
+    {
+        return new ErrorResult(404, message);
+    }
+    public static Result AlreadyExists(string message)
+    {
+        return new ErrorResult(409, message);
+    }
+}
+public class Result<T> : Result
+{
+    public T? Data { get; private set; }
+    public static Result<T> ok(T? data)
+    {
+        return new Result<T>
+        {
+            Success = true,
+            StatusCode = 200,
+            Data = data
+        };
+    }
 }
